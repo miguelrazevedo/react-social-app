cd14a65e2f6f3d0088da34f8eb9d2db8fcfc77b0
Miguel Azevedo <mra.razevedo@gmail.com>
2023-02-13 18:11:34 +0000

    Authentication completed

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,7 +1,8 @@
 import { db } from "../databaseConnection.js";
 import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 /*
-    - Create users
+    - Create user
     POST /api/auth/register
     body:
         username,
@@ -48,13 +49,67 @@ export const register = async (req, res) => {
 };
 
 /*
-    POST
-    Login user
+    - Login user
+    POST /api/auth/login
+    body:
+        username,
+        password
  */
-export const login = (req, res) => {};
+export const login = async (req, res) => {
+  const { username } = req.body;
+
+  // Check if the user exists
+  db.query(
+    "SELECT * FROM users WHERE username = ?",
+    [username],
+    async (err, data) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      // If there isn't any user, send an error response
+      if (data.length === 0) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      // True if passwords are the same
+      const checkPassword = await bcrypt.compare(
+        req.body.password,
+        data[0].password
+      );
+
+      if (!checkPassword) {
+        return res.status(400).json({ message: "Wrong password or username" });
+      }
+
+      // Generate JWT
+      const jwtSecret = process.env.JWT_SECRET;
+      const token = jwt.sign({ id: data[0].id }, jwtSecret);
+
+      // Separate password from the rest of the userObject
+      // Set JWT as a cookie with name "accessToken"
+      const { password, ...userObject } = data[0];
+      res
+        .cookie("accessToken", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(userObject);
+    }
+  );
+};
 
 /*
-    POST
-    Logout user
+    - Logout user
+    POST /api/auth/logout
  */
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  // Clear the cookie that contains the JWT
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json({ message: "User has been logged out" });
+};
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 import Express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 // Express config
 import authRoutes from "./routes/auth.js";
@@ -8,11 +10,14 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import likeRoutes from "./routes/likes.js";
 import commentRoutes from "./routes/comments.js";
+
 const app = Express();
 const PORT = process.env.PORT || 8800;
 
 // Middleware
 app.use(Express.json());
+app.use(cors());
+app.use(cookieParser());
 
 // ROUTES
 app.use("/api/auth", authRoutes);
